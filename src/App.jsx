1746c09d29a5951197678b7882d1de64b8d5ba3b
Gajaa-Azhura <shinr433@gmail.com>
2025-04-22 19:38:12 +0530

    my first command

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./assets/components/Header"
+import Section from "./assets/components/Section"
+import Footer from "./assets/components/Footer"
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Section/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
